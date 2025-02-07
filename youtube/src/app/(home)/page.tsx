e2846b3e9e9d763da6359f0c941944e52095b252
Jitesh Kadiyala <kadiyalajitesh2003@gmail.com>
2025-02-08 00:43:12 +0530

    upto video content

---
+++ youtube/src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Jitesh" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading .........</p>}>
+        <ErrorBoundary fallback={<p>Error oops!!!</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
+}
---
+++ youtube/src/modules/auth/ui/components/auth-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { UserCircleIcon } from "lucide-react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export const AuthButton = () => {
+  return (
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border border-blue-500 rounded-full shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
